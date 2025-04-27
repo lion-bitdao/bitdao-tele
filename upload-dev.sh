48c6f348be03b1a6dd07c44e2202c80ce87a6cea
Lion <lionzhou@me.com>
2025-04-27 16:51:31 +0800

    删除无用文件

--- upload-dev.sh
+++
@@ -1,4 +0,0 @@
-cd dist-dev
-s3cmd put --guess-mime-type * s3://dev-tele.bitdao.com/ --recursive --acl-public
-s3cmd modify --add-header="Content-Type: text/css" s3://dev-tele.bitdao.com/index.css
-cd ..
